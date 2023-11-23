f5fc4bed4232098de46e8169c5495764ee941fd1
kayra <kayrakai8@gmail.com>
2023-11-23 12:40:45 +0100

    brunzynmaster

---
+++ 01B/app.js
@@ -0,0 +1,33 @@
+class App
+{
+    runApplication()
+    {
+        //code
+        this.greeting = "Starting up";
+        this.versienummer = "beta alpha test 0.1"
+        this.Variableswdar = 387
+        this.appNaam = "i dont need no"
+        this.versie = "apetrots"
+        this.versiedatum = "23-234-2049"
+        this.auther = "Bas Bijl"
+        this.copyright = "ja toch niet stelen"
+        this.distributeur = "je weet zelf"
+        this.darkmode = true 
+    
+    }
+}
+
+let app = new App();
+app.runApplication();
+
+
+console.log(app.greeting);
+console.log("versienummer: " + app.versienummer)
+console.log("Variabelwaarde = " + app.Variableswdar)
+console.log("appnaam: " + app.appNaam)
+console.log("versie: " + app.versie)
+console.log("versiedatum: " + app.versiedatum)
+console.log("auther: " + app.auther)
+console.log("copyright: " + app.copyright)
+console.log("distributeur: " + app.distributeur)
+console.log("darkmode = " + app.darkmode)
