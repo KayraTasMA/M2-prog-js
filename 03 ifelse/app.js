2254ee1e1bb00e58561f83bf30c7021951cc16e7
kayra <kayrakai8@gmail.com>
2023-11-30 12:54:54 +0100

    commentaar

---
+++ 03 ifelse/app.js
@@ -0,0 +1,11 @@
+class App
+{
+    runApplication()
+    {
+        //hier komt je code
+        //dit is code commentaar voor javascript
+        //dit is ook commentaar
+    }
+}
+let app = new App();
+app.runApplication();
